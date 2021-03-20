306dc00a413919e628c02bf7e197dbbb25b86ccf
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-20 17:59:33 +0300

    Update declare-javascript-variables.js

--- declare-javascript-variables.js
+++ declare-javascript-variables.js
@@ -1,9 +1,8 @@
-In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, symbol, 
-  bigint, number, and object.
+In computer science, data is anything that is meaningful to the computer. JavaScript provides eight different data types which are undefined, null, boolean, string, 
+  symbol, bigint, number, and object.
 
 For example, computers distinguish between numbers, such as the number 12, and strings, such as "12", "dog", or "123 cats", which are collections of characters. Computers 
-can
-perform mathematical operations on a number, but not on a string.
+can perform mathematical operations on a number, but not on a string.
 
 Variables allow computers to store and manipulate data in a dynamic fashion. They do this by using a "label" to point to the data rather than using the data itself. Any
 of the
