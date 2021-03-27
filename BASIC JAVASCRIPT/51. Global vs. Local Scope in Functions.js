52af6eb04d07a8c722706639cff18f5a926c7f43
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-03-27 14:05:20 +0300

    Create 51. Global vs. Local Scope in Functions.js

---
+++ BASIC JAVASCRIPT/51. Global vs. Local Scope in Functions.js
@@ -0,0 +1,25 @@
+Global vs. Local Scope in FunctionsPassed
+It is possible to have both local and global variables with the same name. When you do this, the local variable takes precedence over the global variable.
+
+In this example:
+
+var someVar = "Hat"; function myFun() { var someVar = "Head"; return someVar; }
+The function myFun will return the string Head because the local version of the variable is present.
+
+Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+Solution:
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  var outerWear = "sweater";
+
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
