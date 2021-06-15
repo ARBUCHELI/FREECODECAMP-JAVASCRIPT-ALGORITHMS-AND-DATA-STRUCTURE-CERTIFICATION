46c1a4c55f371d69843527b29d4e0d4b13554ff5
ANDRÉS R. BUCHELI (Andrés F. Regalado Bucheli) <radio_seattle@hotmail.com>
2021-06-15 11:31:42 +0300

    Update 17. Create Strings using Template Literals.js

--- ES6/17. Create Strings using Template Literals.js
+++ ES6/17. Create Strings using Template Literals.js
@@ -33,3 +33,21 @@ Use an iterator method (any kind of loop) to get the desired output (shown below
 
 Solution:
 
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
+function makeList(arr) {
+  // Only change code below this line
+  const failureItems = [];
+  for (let i = 0; i < (result.failure).length; i++) {
+    failureItems.push(`<li class="text-warning">${result. failure[i]}</li>`);
+  };
+
+  // Only change code above this line
+
+  return failureItems;
+}
+
+const failuresList = makeList(result.failure);
